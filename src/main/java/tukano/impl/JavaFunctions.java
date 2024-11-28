77f4fbdec1b150acbd470c965e8ee22d2996d622
Eduardo Ervideira <ejcept@gmail.com>
2024-11-28 14:10:56 +0000

    feat: implement JavaFunctions with stats method

--- src/main/java/tukano/impl/JavaFunctions.java
+++ src/main/java/tukano/impl/JavaFunctions.java
@@ -1,10 +1,32 @@
 package tukano.impl;
 
-import jakarta.ws.rs.Path;
+import tukano.api.Functions;
+import tukano.api.Result;
+import utils.JSON;
 
-public class Functions {
-    @Path("/stats")
-    public static String stats() {
-        return "Hello, World!";
+import java.util.logging.Logger;
+
+public class JavaFunctions implements Functions {
+
+    private static Logger Log = Logger.getLogger(JavaFunctions.class.getName());
+
+    private static Functions instance;
+
+    synchronized public static Functions getInstance() {
+        if( instance == null )
+            instance = new JavaFunctions();
+        return instance;
+    }
+
+    private JavaFunctions() {}
+
+    @Override
+    public Result<String> stats(String shortId) {
+        Log.info(() -> "stats\n");
+
+        //DB.sql();
+        // UPDATE stats SET views = views + 1 WHERE shortId = ?
+
+        return Result.ok(JSON.encode("shortId" + shortId));
     }
 }
