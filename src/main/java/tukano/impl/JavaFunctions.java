bd2be326494f75d13c71bc1ed340c102d3984651
Eduardo Ervideira <ejcept@gmail.com>
2024-11-28 17:24:07 +0000

    feat: attempt to update views using transactions

--- src/main/java/tukano/impl/JavaFunctions.java
+++ src/main/java/tukano/impl/JavaFunctions.java
@@ -2,9 +2,16 @@ package tukano.impl;
 
 import tukano.api.Functions;
 import tukano.api.Result;
+import tukano.api.Short;
+import utils.DB;
 import utils.JSON;
 
 import java.util.logging.Logger;
+
+import static java.lang.String.format;
+import static tukano.api.Result.*;
+import static tukano.api.Result.ErrorCode.BAD_REQUEST;
+import static utils.DB.getOne;
 
 public class JavaFunctions implements Functions {
 
@@ -22,11 +29,14 @@ public class JavaFunctions implements Functions {
 
     @Override
     public Result<String> stats(String shortId) {
-        Log.info(() -> "stats\n");
+        Log.info(() -> "Increment stats for shortId=\n" + shortId);
 
-        //DB.sql();
-        // UPDATE stats SET views = views + 1 WHERE shortId = ?
-
-        return Result.ok(JSON.encode("shortId" + shortId));
+        return DB.transaction(hibernate -> {
+            return errorOrResult(JavaShorts.getInstance().getShort(shortId), shrt -> {
+                var query = format("UPDATE stats SET views = views + 1 WHERE shortId = :shortId");
+                hibernate.createQuery(query).setParameter("shortId", shortId).executeUpdate();
+                return Result.ok(shortId);
+            });
+        });
     }
 }
