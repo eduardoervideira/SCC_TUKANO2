2b385c4790c4d71f0f1795bf472f2329618b49ef
Eduardo Ervideira <ejcept@gmail.com>
2024-11-28 14:10:10 +0000

    add REST endpoint for functions

--- src/main/java/tukano/api/rest/RestFunctions.java
+++ src/main/java/tukano/api/rest/RestFunctions.java
@@ -1,5 +1,18 @@
 package tukano.api.rest;
 
-public interface Functions {
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 
+@Path(RestFunctions.PATH)
+public interface RestFunctions {
+    String PATH = "/functions";
+    String SHORT_ID = "shortId";
+
+    @GET
+    @Path("/stats")
+    @Produces(MediaType.APPLICATION_JSON)
+    String stats(@QueryParam(SHORT_ID) String shortId);
 }
